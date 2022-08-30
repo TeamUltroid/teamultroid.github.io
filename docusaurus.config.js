89f618869713596db4481c7248bffac30f386664
Devesh Pal <newdev0@outlook.com>
2022-08-30 14:26:32 +0530

    Revert change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -98,14 +98,11 @@ module.exports = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-DF813CEEN9',
+          anonymizeIP: true,
+        }
       },
-    ],
-    [
-      '@docusaurus/plugin-google-gtag',
-      {
-        trackingID: 'G-DF813CEEN9',
-        anonymizeIP: true,
-      }
     ],
   ],
 };
